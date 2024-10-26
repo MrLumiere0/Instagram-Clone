0d6cd33ab069088794f2e76f34b3df9a95d85e27
Louis <hashid.louis@bb-d90wyfmm66.1dc.com>
2024-10-26 15:08:12 -0400

    finished

---
+++ script.js
@@ -0,0 +1,14 @@
+const joinmultiverse = document.getElementsByClassName("joinmultiverse");
+
+console.log(joinmultiverse)
+
+
+function userEvent (){
+    console.log("click")
+}
+
+joinmultiverse.addEventListener("click",userEvent)
+
+
+function userEvent (){
+location.replace("./account.html")}
